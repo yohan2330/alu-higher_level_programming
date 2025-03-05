681e58c06d9c28a9d3f64ba7ffb7c8c9db243066
kouamemoayemorelyohan <m.kouame@alustudent.com>
2025-03-04 22:55:20 -0800

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
+# 1-square.py
+"""Defines a Square class."""
 
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
 
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initialize with size."""
+        self.__size = size

c065a0e2273829b6a4e905535292e5864809b1ed
kouamemoayemorelyohan <m.kouame@alustudent.com>
2025-03-14 13:00:10 -0700

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str1 = "object-oriented "
 str2 = "programming "
-str3 = "with "
-str4 = "Python"
-print(f"{str1}{str2}{str3}{str4}")
+str3 = "with python"
+print(f"{str1}{str2}{str3}")
